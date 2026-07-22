9d8b6b0058530bc31614dd8524eeed66f19fdb4e
NM-DigitalHUB <admin@nm-digitalhub.com>
2026-07-22 18:43:27 +0300

    Make full-screen error fill available space

--- app/src/main/java/me/kalfa/agentconsole/ui/message/MessageComponents.kt
+++ app/src/main/java/me/kalfa/agentconsole/ui/message/MessageComponents.kt
@@ -4,10 +4,10 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHost
@@ -155,7 +155,7 @@ fun FullScreenErrorState(
 ) {
     Column(
         modifier = modifier
-            .fillMaxWidth()
+            .fillMaxSize()
             .padding(24.dp)
             .semantics { liveRegion = LiveRegionMode.Polite },
         horizontalAlignment = Alignment.CenterHorizontally,
