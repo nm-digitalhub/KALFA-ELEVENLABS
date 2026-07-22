5531c6fb878f97a22d2659c6ead4991a2fdfdee7
NM-DigitalHUB <admin@nm-digitalhub.com>
2026-07-22 18:58:50 +0300

    Migrate AppFailure.kt to typed UI results

--- app/src/main/java/me/kalfa/agentconsole/domain/error/AppFailure.kt
+++ app/src/main/java/me/kalfa/agentconsole/domain/error/AppFailure.kt
@@ -8,5 +8,23 @@ sealed interface AppFailure {
     data object Conflict : AppFailure
     data object Validation : AppFailure
     data object RealtimeDisconnected : AppFailure
+    data object CallNoLongerActive : AppFailure
+    data object CampaignHoldRequired : AppFailure
+    data object NoActiveCampaign : AppFailure
+    data object GuestMissingPhone : AppFailure
     data object Unknown : AppFailure
+}
+
+sealed interface AppResult<out T> {
+    data class Success<T>(val value: T) : AppResult<T>
+    data class Failure(val reason: AppFailure) : AppResult<Nothing>
+}
+
+sealed interface RepositoryHealth {
+    data object Loading : RepositoryHealth
+    data object Fresh : RepositoryHealth
+    data class Stale(
+        val reason: AppFailure,
+        val hasCachedData: Boolean
+    ) : RepositoryHealth
 }
